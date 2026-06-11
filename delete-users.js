908c820841b792c0e7b344954c2480aa71f93555
suratp <poolsawat.surat@gmail.com>
2026-06-11 14:00:48 +0700

    Fix Prisma import in delete users script

--- delete-users.js
+++ delete-users.js
@@ -1,16 +1,17 @@
-const { PrismaClient } = require('./generated/prisma/client');
+import { PrismaClient } from './generated/prisma/client';
+
 const prisma = new PrismaClient();
 
 async function main() {
   try {
     const deletedUsers = await prisma.user.deleteMany({});
     console.log('Deleted users:', deletedUsers.count);
-    
+
     const deletedVerifications = await prisma.verification.deleteMany({});
     console.log('Deleted verifications:', deletedVerifications.count);
-    
   } catch (error) {
     console.error('Error:', error);
+    process.exitCode = 1;
   } finally {
     await prisma.$disconnect();
   }
